db679ec5b191e457721d46a90060951612c7df40
SAMUEL VERNECK <nodezeirojs@gmail.com>
2017-04-14 12:10:40 -0300

    betateste

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  pfx: fs.readFileSync('test/fixtures/test_cert.pfx'),
+  passphrase: 'sample'
+};
+
+https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('index.html');
+}).listen(8000);
